1ec65e625cfe570375060e67eb22451b25a5fbae
Clay McIlrath <clay.mcilrath@gmail.com>
2015-09-19 03:05:54 -0400

    twitter config fix, update gems, and manticore

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,10 +16,10 @@ class TweetsController < ApplicationController
 
   def data
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_ACCESS_TOKEN']
-      config.consumer_secret     = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-      config.access_token        = ENV['TWITTER_CONSUMER_KEY']
-      config.access_token_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
 
     respond_to do |format|
